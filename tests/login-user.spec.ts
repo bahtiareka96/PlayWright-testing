c7e1ea2ba0f1e29971b3262e5e1b039e4f8655e5
bahtiar <bahtiareka96@gmail.com>
2025-08-29 17:39:53 +0700

    Add login page test using JSON

--- tests/login-user.spec.ts
+++ tests/login-user.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 import { beforeEach } from 'node:test';
 import { PlaywrightBlocker } from '@ghostery/adblocker-playwright';
 import fetch from 'cross-fetch';
@@ -30,9 +31,23 @@ test.describe('Login Page Test', () => {
         await page.getByRole('textbox', { name: 'Email*' }).fill(user.email);
         await page.getByRole('textbox', { name: 'Password*' }).fill(user.password);
         await page.locator('div').filter({ hasText: /^Sign In$/ }).click();
-        await expect(page.locator('#maincontent > div.page-title-wrapper > h1 > span')).toContainText('Home Page');
+        await expect(page.locator('h1 > span')).toContainText('Home Page');
         // const successMessage = page.locator('.message-success');
         // await expect(successMessage).toBeVisible();
+    });
+
+    test('Login using unregistered user', async ({ page }) => {
+        await page.goto('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/');
+
+        const firstName = faker.person.firstName();
+        const lastName = faker.person.lastName();
+        const email = faker.internet.email({ firstName, lastName });
+        const password = faker.internet.password({ length: 12, pattern: /[A-Za-z0-9!@#$%^&*-]/ });
+
+        await page.getByRole('textbox', { name: 'Email*' }).fill(email);
+        await page.getByRole('textbox', { name: 'Password*' }).fill(password);
+        await page.locator('div').filter({ hasText: /^Sign In$/ }).click();
+        await expect(page.locator('.message-error')).toContainText('The account sign-in was incorrect');
     });
         
 })
